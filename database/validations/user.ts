2a76a4a054599c84de207378cb6390ee7fbd8d2e
pranavcpawar <pranavcpawar@outlook.com>
2024-02-23 03:00:46 +0530

    add validations for empty field

--- database/validations/user.ts
+++ database/validations/user.ts
@@ -6,11 +6,13 @@ export const UserValidation: z.ZodType<IUser> = z
   email: z.string().email(),
   username: z
               .string()
+                .min(1,{message: "Username is required"})
                 .min(3, {message: "Minimum username length is 3 characters"})
                 .max(20, {message: "Maximum username length is 20 characters"}),
 
   password: z
               .string()
+              .min(1, { message: "Password is required" })
               .min(8, { message: "Password is too short" })
               .max(20, { message: "Password is too long" }),
   
