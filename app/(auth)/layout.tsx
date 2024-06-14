6e09dc30ff02f320c64c9e5532d2241e1208f3ae
pranavcpawar <pranavcpawar@outlook.com>
2024-06-14 21:36:07 +0530

    add layout for register page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,13 +1,14 @@
 import type { Metadata } from "next";
-import { Noto_Sans } from "next/font/google";
+import { Comfortaa } from "next/font/google";
 import "../globals.css";
+import Image from "next/image";
 
-const lato = Noto_Sans({ 
+const comfortaa = Comfortaa({ 
   subsets: ["latin"], 
-  weight: ["100", "300", "400", "700", "900"],
+  weight: [ "300", "400", "700"],
   adjustFontFallback: true,
-  style: ["normal", "italic"],
-  variable: "--font-lato",
+  style: ["normal"],
+  variable: "--font-comfortaa",
 });
 
 export const metadata: Metadata = {
@@ -22,9 +23,12 @@ export default function AuthLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`dark sm:flex ${lato.className}`}>
-        {children}
-
+      <body className={`dark flex flex-col items-center justify-between min-h-screen ${comfortaa.className}`}>
+        <div className="flex sm:items-center items-start justify-center w-full">
+          <Image src="/logo.svg" className="p-4 max-h-[380px] h-[50%] w-full hidden md:flex" alt="logo" width={424} height={340} priority />
+          {children}
+        </div>
+        <div className="text-[#9b9b9b] text-xs p-1"><h3>Frienso © {new Date().getFullYear()}</h3></div>
       </body>
     </html>
   );
