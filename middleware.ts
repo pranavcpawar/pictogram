4c9f6647276694795cb940bdd3f16afd4690a70b
pranavcpawar <pranavcpawar@outlook.com>
2024-06-15 20:25:21 +0530

    adds basic middleware config

---
+++ middleware.ts
@@ -0,0 +1,22 @@
+import { cookies } from "next/headers";
+import { NextResponse, type NextRequest } from "next/server";
+
+export function middleware(request: NextRequest){
+
+  const isPublic = ["/login", "/register"].includes(request.nextUrl.pathname);
+
+  const auth = cookies().get("auth")?.value || "";
+
+  if(!isPublic && !auth){
+    return NextResponse.redirect(new URL("/login", request.url));
+  };
+
+  if(isPublic && auth){
+    return NextResponse.redirect(new URL("/", request.url));
+  };
+
+};
+
+export const config = {
+  matcher: ["/", "/login", "/register"],
+};
