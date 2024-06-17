cc4ea8b7beec1c5b45b2a7a00f68f4d6eb3116ff
pranavcpawar <pranavcpawar@outlook.com>
2024-06-17 16:20:19 +0530

    adds submit button

---
+++ components/submit-button.tsx
@@ -0,0 +1,25 @@
+import { LuLoader2 } from "react-icons/lu";
+
+export default function SubmitButton({
+  children,
+  isLoading,
+  onClick,
+  submitText
+} : {
+  children: React.ReactNode;
+  isLoading: boolean;
+  onClick?: (e: React.MouseEvent<HTMLButtonElement, globalThis.MouseEvent>) => void;
+  submitText?: string;
+}
+){
+
+  return (
+    <button 
+      disabled={isLoading} 
+      onClick={onClick}
+      className="w-2/3 bg-[#CA2C92] hover:bg-[rgb(202,44,146,0.9)] active:scale-95 text-black font-bold flex flex-col space-y-[3px] items-center justify-center p-2 rounded-[8px]">
+        {isLoading && <LuLoader2 className="animate-spin" />}
+        {isLoading ? submitText : children} 
+    </button>
+  );
+};
