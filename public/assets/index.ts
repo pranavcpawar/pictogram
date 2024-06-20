2b4481b0621acb34ff94cfd6ba419a18eec4c239
pranavcpawar <pranavcpawar@outlook.com>
2024-06-21 00:11:35 +0530

    adds navigation logos

---
+++ public/assets/index.ts
@@ -0,0 +1,9 @@
+import add from "./add.svg"
+import heart from "./heart.svg"
+import home from "./home.svg"
+import profile from "./profile.svg"
+import search from "./search.svg"
+import send from "./send.svg"
+import ham from "./ham.svg"
+
+export { add, heart, home, profile, search, send, ham };
