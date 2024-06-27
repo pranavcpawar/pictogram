e3da99bc9da43101a14285ad7948cfef561d226b
pranavcpawar <pranavcpawar@outlook.com>
2024-06-27 16:17:14 +0530

    clean home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,21 +1,7 @@
-"use client";
-import SubmitButton from "@/components/submit-button";
-import { useAction } from "next-safe-action/hooks";
-import { logoutUserAction } from "../actions/form-actions";
-import { useRouter } from "next/navigation";
-import { useTransition } from "react";
-
 export default function Home() {
-  const router = useRouter();
-  const [isPending, startTransition] = useTransition();
-  const { execute, status } = useAction(logoutUserAction, {
-    onSuccess() {
-      router.push("/login");
-    }
-  });
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <SubmitButton onClick={() => startTransition(() => execute({ auth: "session" }))} isLoading={status === "executing" || isPending}>Logout</SubmitButton>
+    <main className="w-full min-h-screen flex flex-col items-center justify-center gap-40">
+      <h3>Home</h3>
     </main>
   );
 }
