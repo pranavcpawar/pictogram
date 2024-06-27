4d0769ba9e25e1f6671a4ee2e1798d0d7eb04ea2
pranavcpawar <pranavcpawar@outlook.com>
2024-06-27 16:18:07 +0530

    route for inital message page

---
+++ app/(root)/direct/inbox/page.tsx
@@ -0,0 +1,7 @@
+export default function InboxPage() {
+  return(
+    <main className="w-full min-h-screen flex flex-col items-center justify-center gap-40">
+      <h3>Inbox</h3>
+    </main>
+  );
+};
